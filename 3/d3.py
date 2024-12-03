814352509d6181b7264cddc4e72ff3f6acdd4c0e
Whizma <=>
2024-12-03 11:47:13 +0100

    Day3 p2 done

--- 3/d3.py
+++ 3/d3.py
@@ -20,19 +20,31 @@ def p2():
   
   with open('input.txt', 'r') as file:
     instructions = [line.strip() for line in file]
-    pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
-    do_pattern = r'do\(\)'
-    dont_pattern = r"don't\(\)"
+    combined_pattern = (
+        r'(?P<mul>mul\((\d{1,3}),(\d{1,3})\))|'
+        r'(?P<do>do\(\))|'
+        r'(?P<dont>don\'t\(\))'
+    )    
     mul = True
-    
     res = 0
     for line in instructions:
-      matches = re.finditer(pattern, line)
+      matches = re.finditer(combined_pattern, line)
       for match in matches:
-        a = int(match.group(1))
-        b = int(match.group(2))
-        res += a * b
-      
+        print(match.group(2))
+        if match.group('mul'):
+          if mul:
+            a = int(match.group(2))
+            b = int(match.group(3))
+            res += a * b
+          else:
+            print("yurr")
+        elif match.group('do'):
+          print('do')
+          mul = True
+        elif match.group('dont'):
+          print('dont')
+          mul = False
+        
     print(res)
 
 
